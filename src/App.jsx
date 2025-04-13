29337e7bb7eb5a61f8d923485f43c7c746ef2d33
Mohammad (Sani) Bello <69519271+Lycan-Xx@users.noreply.github.com>
2025-04-13 12:06:53 -0700

    Fixed the routing error in github pages by replacing 'BrowserRouter with 'HashRouter' it works best when working with static webpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/Dashboard';
 import Subscribers from './components/Subscribers';
